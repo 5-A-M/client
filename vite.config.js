929933da1fb986430ecbce64acd36839c6afee7a
ddinggu <officerudwls@gmail.com>
2023-06-09 10:00:42 +0900

    압축 파일 빌드 설정 제거
    S3 정적 웹 호스팅 서비스에서 .gz 파일 적용에 어려움이 있어 vite 빌드
    설정 수정
    - 관련 라이브러리: vite-plugin-compression2

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import eslint from 'vite-plugin-eslint';
-import { compression } from 'vite-plugin-compression2';
 
 export default defineConfig(({ mode }) => ({
-	plugins: [
-		react(), 
-		eslint(), 
-		svgr({ svgrOptions: { icon: true } }),
-		compression({ include: /\.(js|css|html|svg|json)(\?.*)?$/i, threshold: 1024 })
-	],
-	esbuild: {
-		pure: mode === 'production' ? ['console.log'] : [],
-	},
+	plugins: [react(), eslint(), svgr({ svgrOptions: { icon: true } })],
 }));
